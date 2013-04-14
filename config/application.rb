c72495a6bf103c3532de8ff6f399c0c285f21ff4
MichaelRoss <msross@dundee.ac.uk>
2013-04-14 22:15:46 +0100

    trying mobvious

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module Website
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.middleware.use Mobvious::Manager
   end
 end
